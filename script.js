87786c491a052b7fdbc73fcb337e246f093d1217
Loupdo <188009481+Loupdo@users.noreply.github.com>
2024-12-15 16:24:06 -0500

    finish logic and added size FINISHED

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const container=document.querySelector(".container")
-for(j=0; j<16; j++){
-line=document.createElement('div')
-for (i=0;i<16;i++){
-    div=document.createElement('div')
-    div.classList.add("colored")
-    div.style.width="40px"
-    div.style.height="40px"
-    line.appendChild(div)
-    line.style.display="flex"
-    line.style.width="fit-content"
-    }
-container.appendChild(line)
+const coloring = () => {
+    const toColor=document.querySelectorAll(".colored")
+    console.log(toColor)
+    toColor.forEach((box)=>{
+       box.addEventListener("mouseover",() => {box.style.backgroundColor="black"})
+    })
 }
-const toColor=document.querySelectorAll(".colored")
-toColor.forEach((box)=>{
-   box.addEventListener("click",() => {box.style.backgroundColor="black"})
-})
+
+const newGrid= (size) => {
+    for(j=0; j<size; j++){
+        line=document.createElement('div')
+        for (i=0;i<size;i++){
+            div=document.createElement('div')
+            div.classList.add("colored")
+            div.style.width="40px"
+            div.style.height="40px"
+            line.appendChild(div)
+            line.style.display="flex"
+            line.style.width="fit-content"
+        }
+    container.appendChild(line)
+    }
+    coloring()
+}
+
+const createGrid = () => {
+    container.innerHTML= null
+    const numberUser = prompt("enter a number between 1-100")
+    if (numberUser <=100 && numberUser > 0){ newGrid(numberUser)}
+}
+
+const container=document.querySelector(".container")
+const sizeBtn=document.querySelector(".size-btn")
+
+sizeBtn.addEventListener("click", () => createGrid())
+newGrid(16)
